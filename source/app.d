cd1d592f5cc3ea0b5df00a18e8f9c070de199f23
Pavel Chebotarev <chebotarevp@gmail.com>
2017-04-01 21:31:02 +0300

    Added help information and run options

--- source/app.d
+++ source/app.d
@@ -1,12 +1,60 @@
-import std.stdio;
+import std.stdio, std.getopt;
 import socks5d.server;
 
-int main()
-{
-    writeln("Starting socks5d server v. 0.0.1");
+immutable string versionString = "0.0.1";
+immutable string defaultAddress = "127.0.0.1";
+immutable ushort defaultPort = 1080;
 
-    auto server = new Server(1080);
-    server.start();
+ushort port = defaultPort;
+string address = defaultAddress;
+bool   verbose;
+bool   ver;
+
+int main(string[] args)
+{
+    if (processHelpInformation(args)) {
+        return 0;
+    }
+
+    startServer(address, port);
 
     return 0;
+}
+
+void startServer(string address, ushort port)
+{
+    writefln("Starting socks5d server v. %s", versionString);
+
+    auto server = new Server(address, port);
+    server.start();
+}
+
+
+bool processHelpInformation(string[] args)
+{
+    import std.conv;
+    const string helpString = "Socks5d SOCKS 5 proxy server version " ~ versionString ~ ".\n\n" ~
+        "Usage: socks5d [OPTIONS]";
+
+    auto helpInformation = getopt(args,
+        std.getopt.config.caseSensitive,
+        "address", "Address to bind to (" ~ defaultAddress ~ " by default).",   &address,
+        "port",    "Port number to listen to (" ~ to!string(defaultPort) ~ " by default).", &port,
+
+        "version|V",   "Print version and exit.",    &ver,
+        "verbose|v", "Use verbose output.",        &verbose,
+    );
+
+    if (ver) {
+        writefln("Socks5d version %s", versionString);
+
+        return true;
+    }
+
+    if (helpInformation.helpWanted) {
+        defaultGetoptPrinter(helpString, helpInformation.options);
+        return true;
+    }
+
+    return false;
 }
--- source/socks5d/server.d
+++ source/socks5d/server.d
@@ -2,19 +2,21 @@ module socks5d.server;
 
 import socks5d.client;
 import core.thread : Thread;
-import std.socket;
+import std.socket, std.stdio;
 
 class Server : Thread
 {
     private:
+        string address;
         ushort port;
         int backlog;
         Socket socket;
         Client[] clients;
 
     public:
-        this(ushort port = 1080, int backlog = 10)
+        this(string address, ushort port = 1080, int backlog = 10)
         {
+            this.address = address;
             this.port = port;
             this.backlog = backlog;
 
@@ -25,8 +27,10 @@ class Server : Thread
         {
             socket = new TcpSocket;
             assert(socket.isAlive);
-            socket.bind(new InternetAddress(port));
+            socket.bind(new InternetAddress(address, port));
             socket.listen(backlog);
+
+            writefln("Listening on %s:%d", socket.localAddress().toAddrString(), port);
 
             while (true) {
                 acceptClient();
